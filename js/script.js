5ce750c39fd04d5a3ab45ae6f7f907ab90495c91
Tyler M <92123605+Tyler-Zorb@users.noreply.github.com>
2022-11-29 09:36:11 -0600

    Set up the API

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
+const getImage = async function () { //creating async function
+    const res = await fetch ("https://picsum.photos/v2/list?limit=100"); //providing the function with the API URL.
+    const images = await res.json(); //parse the data captured in the res variable using the .json() have to use the await kw.
+    console.log(images);
+};
+
+getImage();
